cfe87a4bc789d7c043f8fbc29a7afd83d93b2533
m-lukas <mail@lukasmueller.de>
2019-01-22 17:33:29 +0100

    Style search popup

--- utils.js
+++ utils.js
@@ -1,8 +1,10 @@
 closePopup = () => {
 
     let confirmation_overlay = document.getElementById('confirmation-overlay');
+    let search_overlay = document.getElementById('search-overlay');
 
     if(confirmation_overlay.classList.contains('is-visible')) confirmation_overlay.classList.remove('is-visible');
+    if(search_overlay.classList.contains('is-visible')) search_overlay.classList.remove('is-visible');
     App.states.isMoving = false;
     App.states.movingCoords = null;
     resetOV();
@@ -17,5 +19,11 @@ cancelMoving = () => {
     App.states.isMoving = true;
     App.states.movingCoords = null;
     if(confirmation_overlay.classList.contains('is-visible')) confirmation_overlay.classList.remove('is-visible');
+
+}
+
+openSearch = () => {
+
+    document.getElementById('search-overlay').classList.add('is-visible');
 
 }
